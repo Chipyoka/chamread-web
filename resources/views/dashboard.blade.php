32a6d4167d2f16eb4c014c38e579736a3a8bfdcf
Chipyoka <mwapechipyoka18@gmail.com>
2026-03-28 14:28:20 +0200

    jkjk

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
             <div class="text-gray-600 mb-6">
                 <h4 class="text-2xl font-medium">Welcome,</h4>
                 <p class="text-sm">Below are summaries of key metrics from the overall Chamread performance.</p>
-                <a href="{{route('admin.csas.index')}}">Go to CSA</a>
+            
             </div>
             <div class="grid grid-cols-3 gap-x-4 gap-y-8">
 
