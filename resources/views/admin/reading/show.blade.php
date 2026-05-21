868d97555e8ddaf0380626d6419a449f20780022
Chipyoka <mwapechipyoka18@gmail.com>
2026-05-21 02:56:04 +0200

    Added photo view

--- resources/views/admin/reading/show.blade.php
+++ resources/views/admin/reading/show.blade.php
@@ -68,7 +68,20 @@
          <!-- reading Info Card -->
         <div class="bg-white rounded-md p-6 space-y-4 border border-gray-200">
          <h3 class="text-gray-400 text-xs uppercase my-2">Readings Information</h3>
-            <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+         <div class="flex justify-start items-start gap-x-4">
+            <div class="w-1/2">
+                @if(!$reading->photo_path)
+                    <div class="flex flex-col gap-4 items-center justify-center  border border-gray-100 rounded-sm bg-gray-50/70 min-h-[380px]">
+                        <i data-lucide="image" class="w-8 h-8 text-gray-300"></i>
+                        <p class="text-gray-400 text-xs">This reading has no image</p>
+                    </div>
+                @else
+                    <div class="h-[380px] max-h-[380px] max-w-[500px] overflow-hidden">
+                        <img src="{{ asset('storage/' . $reading->photo_path) }}" class="h-full object-cover" alt="Reading Photo">
+                    </div>
+                @endif
+            </div>
+            <div class="grid grid-cols-1 md:grid-cols-1 w-full gap-4">
                 <div class="px-2 py-1.5 bg-gray-50 rounded-sm">
                     <h2 class="text-gray-500 mb-1 text-xs uppercase font-normal">Previous Reading</h2>
                     <p class="text-gray-600 font-semibold">{{ number_format((float) ($reading->previous_reading ?: 0), 3) }}</p>
@@ -99,11 +112,16 @@
                         {{ $value?? 'Not provided' }}
                     </p>
                 </div>
+                  <div class="px-2 py-1.5 bg-gray-50 rounded-sm">
+                    <h2 class="text-gray-500 mb-1 text-xs uppercase font-normal">Reading Time</h2>
+                    <p class="text-gray-600 font-semibold"> {{ $reading->reading_time->format('Y-m-d H:i:s') ?? '-' }}</p>
+                </div>
 
            
 
              
             </div>
+         </div>
         </div>
 
         <!-- Customer Info Card -->
@@ -156,6 +174,7 @@
              
             </div>
         </div>
+
 
     </div>
 </x-app-layout>
